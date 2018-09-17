c931a8278d546551c5099d01eb4db1214027c0fa
Maxim Sukharev <maxim@sourced.tech>
2018-09-17 15:10:30 +0200

    Use midi device instead of sonic-pi

--- src/sound/scale.go
+++ src/sound/scale.go
@@ -1,6 +1,9 @@
 package sound
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 type Scale struct {
 	Notes []string
@@ -19,8 +22,8 @@ func NewScale(notes []string, base, width int) Scale {
 	return Scale{scale}
 }
 
-func (c *Scale) Get(number uint32) string {
-	return c.Notes[number%uint32(len(c.Notes))]
+func (c *Scale) Get(number uint32) int {
+	return keys[c.Notes[number%uint32(len(c.Notes))]]
 }
 
 var (
@@ -28,3 +31,31 @@ var (
 	AMajor = []string{"A", "B", "Cs", "D", "E", "Fs", "Gs"}
 	CMinor = []string{"C", "D", "Es", "F", "G", "As", "Bs"}
 )
+
+var keyBase = map[string]int{
+	"C": 24,
+	"D": 26,
+	"E": 28,
+	"F": 29,
+	"G": 31,
+	"A": 33,
+	"B": 35,
+}
+
+var keys map[string]int
+
+func init() {
+	keys = make(map[string]int)
+	for octave := 0; octave < 9; octave++ {
+		octaveStr := ""
+		if octave != 3 {
+			octaveStr = strconv.Itoa(octave + 1)
+		}
+		for bk, bv := range keyBase {
+			key := bv + (octave * 12)
+			keys[bk+"b"+octaveStr] = key - 1
+			keys[bk+octaveStr] = key
+			keys[bk+"s"+octaveStr] = key + 1
+		}
+	}
+}
